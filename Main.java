e42808baddfe5f7026d52d5a3f890f21d97f9825
suha20 <108548723+suha20@users.noreply.github.com>
2024-11-21 14:35:41 +0000

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-//public class Main {
-    //public static void main(String[]args) {
-        //Person person = new Person("Markus", 50, 300, 150,"markus42@gmaailo.com");
-        //System.out.println("Hello world!" + person.toString());
-    //}
-//}
